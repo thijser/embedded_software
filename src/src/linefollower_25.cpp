63ece7634b28d14c68e09dbd2c6bc256097f34a7
thijser <thijsboumans@online.nl>
2015-01-15 09:46:54 +0100

    as you wish

--- src/src/linefollower_25.cpp
+++ src/src/linefollower_25.cpp
@@ -22,9 +22,10 @@ using namespace std;
 geometry_msgs::Twist twistmsg;//
 
 
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-	cv::Mat img = cv_bridge::toCvCopy(msg, "bgr8")->image;// picture now stored in img
+{	ROS_INFO("yhea?");
+/*	cv::Mat img = cv_bridge::toCvCopy(msg, "bgr8")->image;// picture now stored in img
 	
 	float scale = 0.2;//scale the picture
 	float ps = 0.35;//perspective scaler TWEAK THIS TO MATCH ROBOT SETUP, value probably between 0.3 and 0.4, higher values correct greater angles between phone and ground 0.35 
@@ -53,14 +54,30 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 	 //reduce noise 
 	cv::Mat blur;
+	cv::Mat gray;
 	double sigmaY=0;
 	int borderType=1;
 	GaussianBlur(out,blur,cv::Size(3,3),3,sigmaY,borderType );
-
+	cvtColor(blur,gray,CV_BGR2GRAY);
+	cv::normalize(gray, gray, 0, 255, 1, CV_8UC1);
+	double tot=0;
+	double alhas=0;
+	for(int i=0;i<blur.rows-1;i++){
+		for(int j=0;j<blur.cols-1;j++){
+			float loc=gray.at<uchar>(j,i);
+			float hor=gray.at<uchar>(j+1,i);
+			float vert=gray.at<uchar>(j,i+1);
+			float str= abs(loc)+abs(hor);
+			float hD=hor-loc; //horizontal change 
+			float vD=vert-loc;//vertical change
+			 alhas=alhas+(atan(hD/vD))*str;
+			tot=tot+str;
+		}
+	}
+	float alpha=alhas/tot;
+	printf("alpha= %f",alpha);
 	
-	
-	
-	cv::Mat detected_edges, linewindow;
+/*	cv::Mor at detected_edges, linewindow;
 
 	cv::Canny(blur, detected_edges, 50, 200,3);//edge detection canny
 	int crop = 5;
@@ -146,6 +163,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 int main(int argc, char **argv)
 {
+
 	ros::init(argc, argv, "linefollower_25");//initialise
 	ros::NodeHandle n; //nodehandle
 	ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10); //publish geometry_msgs::Twist on topic /cmd_vel with buffer of 10 messages
